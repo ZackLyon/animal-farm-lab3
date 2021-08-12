ffd53a804da68206917399e406de8db944126fd1
Zack Lyon <zack.j.lyon@gmail.com>
2021-08-12 11:02:13 -0700

    refactored to use functions, complied with eslints demands

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // import functions and grab DOM elements
-const dogPic = document.getElementById("dog-pic");
-const bark = document.getElementById("bark");
-const catPic = document.getElementById("cat-pic");
-const meow = document.getElementById("meow");
-const horsePic = document.getElementById("horse-pic");
-const neigh = document.getElementById("neigh");
-const lastPicked = document.getElementById("last-picked");
+const dogPic = document.getElementById('dog-pic');
+const bark = document.getElementById('bark');
+const catPic = document.getElementById('cat-pic');
+const meow = document.getElementById('meow');
+const horsePic = document.getElementById('horse-pic');
+const neigh = document.getElementById('neigh');
+const lastPicked = document.getElementById('last-picked');
 // initialize state
 
 // set event listeners 
@@ -13,18 +13,38 @@ const lastPicked = document.getElementById("last-picked");
 // use user input to update state 
 // update DOM to reflect the new state
 
+
+function soundAndTextChange(sound, animal) {
+    sound.play();
+    lastPicked.textContent = animal + ' was last picked';
+}
+
 dogPic.addEventListener('click', () => {
-  bark.play();
-  lastPicked.textContent = "Dog was last picked.";
+    soundAndTextChange(bark, 'Dog');
 });
 
 catPic.addEventListener('click', () => {
-  meow.play();
-  lastPicked.textContent = "Cat was last picked.";
+    soundAndTextChange(meow, 'Cat');
 });
 
 horsePic.addEventListener('click', () => {
-  neigh.play();
-  lastPicked.textContent = "Horse was last picked.";
+    soundAndTextChange(neigh, 'Horse');
 });
 
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'd'){
+        soundAndTextChange(bark, 'Dog');
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'c'){
+        soundAndTextChange(meow, 'Cat');
+    }
+})
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'h'){
+        soundAndTextChange(neigh, 'Horse');
+    }
+})
