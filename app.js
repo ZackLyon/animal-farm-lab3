4b3d25ce813395c6e6f5cde7a6d7c15849f69a0e
Zack Lyon <zack.j.lyon@gmail.com>
2021-08-11 17:32:12 -0700

    add animals, associated sounds, arranged them horizontally, rounded corners

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 // import functions and grab DOM elements
-
+const dogPic = document.getElementById("dog-pic");
+const bark = document.getElementById("bark");
+const catPic = document.getElementById("cat-pic");
+const meow = document.getElementById("meow");
+const horsePic = document.getElementById("horse-pic");
+const neigh = document.getElementById("neigh");
 // initialize state
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
+
+dogPic.addEventListener('click', () => {
+  bark.play();
+});
+
+catPic.addEventListener('click', () => {
+  meow.play();
+});
+
+horsePic.addEventListener('click', () => {
+  neigh.play();
+});
